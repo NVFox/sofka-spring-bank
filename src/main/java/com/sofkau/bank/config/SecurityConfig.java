d077d8f3c406a8da5d05835c0fbf2fab7794c100
Andrés Téllez <andres.tellez@sofka.com.co>
2025-01-08 04:06:52 -0500

    config: add config to access h2 console

--- src/main/java/com/sofkau/bank/config/SecurityConfig.java
+++ src/main/java/com/sofkau/bank/config/SecurityConfig.java
@@ -1,19 +1,13 @@
 package com.sofkau.bank.config;
 
-import com.sofkau.bank.services.clients.ClientService;
-import com.sofkau.bank.services.users.auth.UserAuthServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,12 +15,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  {
-    private final ClientService clientService;
-
-    public SecurityConfig(ClientService clientService) {
-        this.clientService = clientService;
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
@@ -35,8 +23,13 @@ public class SecurityConfig  {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(requests ->
                         requests
+                                .requestMatchers("/h2-console").permitAll()
+                                .requestMatchers("/h2-console/**").permitAll()
+                                .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/user/auth/signup").permitAll()
                                 .anyRequest().authenticated()
                 )
