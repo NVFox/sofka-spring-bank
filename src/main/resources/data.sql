058b18890b6eca339347d72bed1bba2a8927e2d0
Andrés Téllez <andres.tellez@sofka.com.co>
2025-01-08 04:56:19 -0500

    chore: add base sql data to spring app

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO account_types
+VALUES
+    (1, 'CHECKING'),
+    (2, 'SAVINGS');
+
+INSERT INTO transaction_actions
+VALUES
+    (1, 'DEPOSIT'),
+    (2, 'WITHDRAWAL'),
+    (3, 'SENT_TRANSFER'),
+    (4, 'RECEIVED_TRANSFER');
