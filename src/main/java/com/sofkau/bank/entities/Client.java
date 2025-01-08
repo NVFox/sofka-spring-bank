f5ee6de45818ba05a5bad3b6d102a17e05dceae2
Andrés Téllez <andres.tellez@sofka.com.co>
2025-01-08 01:03:48 -0500

    feat: add user details impl to client entity

--- src/main/java/com/sofkau/bank/entities/Client.java
+++ src/main/java/com/sofkau/bank/entities/Client.java
@@ -1,6 +1,10 @@
 package com.sofkau.bank.entities;
 
 import java.math.BigDecimal;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -13,7 +17,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "clients")
-public class Client {
+public class Client implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
@@ -107,5 +111,40 @@ public class Client {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
